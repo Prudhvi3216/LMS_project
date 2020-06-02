331592ef140004a61abf02bb55228f78a3a50028
Mallavarapu Prudhvi <mallavarapuprudhvi@gmail.com>
2020-06-02 13:22:47 +0530

    test

--- resources/js/app.js
+++ resources/js/app.js
@@ -103,11 +103,6 @@ Vue.component('new-category', NewCategory);
 //Backend component register
 Vue.component('menu-categories', MenuCategories);
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
 
 const app = new Vue({
     el: '#app',
